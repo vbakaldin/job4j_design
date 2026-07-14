00abbdc04d3f28a1f197caef16288ebed9d3fc53
vladimir <tintutin@list.ru>
2026-07-14 22:24:16 +0400

    User

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +14,27 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>(16);
+        Calendar birthday = Calendar.getInstance();
+
+        User user1 = new User("Alex", 2, birthday);
+        int hashCode1 = user1.hashCode();
+        int hash1 = hashCode1 ^ (hashCode1 >>> 16);
+        int bucket1 = hash1 & 15;
+
+        User user2 = new User("Alex", 2, birthday);
+        int hashCode2 = user2.hashCode();
+        int hash2 = hashCode2 ^ (hashCode2 >>> 16);
+        int bucket2 = hash2 & 15;
+
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+
+        System.out.printf("user1, %s, %s, %s", hashCode1, hash1, bucket1);
+        System.out.println();
+        System.out.printf("user2, %s, %s, %s", hashCode2, hash2, bucket2);
     }
 }
