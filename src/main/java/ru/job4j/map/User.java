545a393451fcfb33cec385b984db7692e2e652e4
vladimir <tintutin@list.ru>
2026-07-15 14:24:09 +0400

    User

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -37,12 +37,12 @@ public class User {
         System.out.println(map.size());
     }
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (o == null || getClass() != o.getClass()) return false;
-//        User user = (User) o;
-//        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
-//    }
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
 
     @Override
     public int hashCode() {
