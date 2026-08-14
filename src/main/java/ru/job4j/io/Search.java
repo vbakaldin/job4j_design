f3fc9c217d3fce9e7286aed4c7c12be0453b524c
vladimir <tintutin@list.ru>
2026-08-14 12:42:33 +0400

    Search

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,8 +9,8 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("C:\\projects");
-        search(start, path -> path.toFile().getName().endsWith(".txt")).forEach(System.out::println);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
