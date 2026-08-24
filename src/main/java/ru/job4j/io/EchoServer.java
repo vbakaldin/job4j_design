a24e0812f785f6871f6376eb97e8993ebcaa622a
vladimir <tintutin@list.ru>
2026-08-24 18:28:48 +0400

    EchoServer

---
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            while (!server.isClosed()) {
+                Socket socket = server.accept();
+                try (OutputStream output = socket.getOutputStream();
+                     BufferedReader input = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+
+                    String request = input.readLine();
+                    boolean exit = request != null && request.contains("msg=Bye");
+
+                    for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
+                        System.out.println(string);
+                    }
+                    output.flush();
+
+                    if (exit) {
+                        server.close();
+                    }
+                }
+            }
+        }
+    }
+}
