8113b0bde7a6388d72d818f281c6b51b5db71b55
vladimir <tintutin@list.ru>
2026-08-25 22:34:09 +0400

    UsageLog4j - 3

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,18 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        byte byteValue = 1;
+        short shortValue = 2;
+        int intValue = 3;
+        long longValue = 4L;
+        float floatValue = 5.5F;
+        double doubleValue = 6.6;
+        char charValue = 'A';
+        boolean booleanValue = true;
+        LOG.debug("name: {}, age: {}, byte: {}, short: {}, int: {}, long: {}, float: {}, double: {}, char: {}, boolean: {}",
+                name, age, byteValue, shortValue, intValue, longValue, floatValue, doubleValue, charValue, booleanValue
+        );
     }
 }
